777dc0b92d312e592e0f03270717bc50eab6dbc9
lach <lachlan@decks-awash.com>
2026-09-25 14:24:18 +1000

    working on typing handle roll
    figuring out states, avoiding null state is hard, i think i need to give
    it an initial state, there's basically 3 options: i was never changed, i
    was changed to a bad state, i was changed to a good state

--- t_ttrpg_frontend/app/actions/handleRoll.ts
+++ t_ttrpg_frontend/app/actions/handleRoll.ts
@@ -1,26 +1,50 @@
 // app/actions/handleRoll.ts
 "use server";
-export async function handleRoll(formData) {
-  formData.preventDefault();
 
-  const rollString = formData.get("rollBox");
+import { error } from "console";
+import { attemptToUpgradeSegmentFromBFCache } from "next/dist/client/components/segment-cache/cache";
 
-  const query = `
-      {
-        "query": "query { roll(tokens: $input: rollString) }"
-      }
-      
-    `;
-  const inputVar = {
-    input: { rollString },
+interface RollData {
+  roll: String[];
+}
+
+
+type RollReturnState =
+  | { sucess: true; data: {roll: string[]}}
+  | { sucess: false; error: string };
+  
+type RollInputValue =
+  | { sucess: true; rollString: string }
+  | { sucess: false; error: string };
+
+  
+export async function handleRoll(formData: FormData): Promise<RollReturnState> {
+  const formValue = formData.get("rollBox");
+
+  const rollString: RollInputValue = typeof formValue
+    === "string"
+    && formValue.trim().length > 0
+    ? {sucess: true, rollString: formValue}
+    : {sucess: false, error: "Invalid input: 'rollBox' must be a non-empty string."}
+   
+  var returnState: RollReturnState =
+  {sucess: false, error: "refused to try because: ", rollString.error}
+  
+  if (!rollString.sucess) {
+    return
+  }
+  const query = `{roll(tokens: "${rollString}")}`;
+
+  const variables = {
+    tokens: rollString,
   };
   try {
     const response = await fetch("http://localhost:8088/graphql/", {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-Type": "Application/json",
       },
-      body: JSON.stringify({ query, inputVar }),
+      body: JSON.stringify({ query, variables }),
     });
 
     const result = await response.json();
